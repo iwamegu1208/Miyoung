b41d4f829266f80859c6647656caad925d6610c7
iwamegu1208 <iwamegu1208@yahoo.co.jp>
2020-04-03 21:52:01 +0900

    Robot 클래스를 만들었습니다.

---
+++ Robot.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of class Robot here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Robot
+{
+    public static void main(String[] args){
+    }
+}
