fd8a5c8bbd20fb789006505b54d9342b57817ade
shka88 <lky1732@gmail.com>
2020-04-03 22:39:59 +0900

    new 버전입니당

--- Robot.java
+++ Robot.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 /**
  * Write a description of class Robot here.
  *
@@ -8,5 +8,21 @@
 public class Robot
 {
     public static void main(String[] args){
+        double speed = 10;
+        String color;
+        Scanner scanner = new Scanner(System.in);
+        
+        //robot = RobotWalk();
+    }
+
+    public static void RobotWalk(String color, double speed)
+    {
+       if(color == "green")
+           System.out.println("동작을 멈춥니다.");
+       else if(color == "yellow")
+           System.out.println("보행속도를 " + speed*1.2 + "km/h로 걷습니다.");
+       else
+           System.out.println("동작을 멈춥니다.");
     }
 }
+
