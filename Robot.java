ae04ab68f617649898b4a53342474016d4e90383
iwamegu1208 <iwamegu1208@yahoo.co.jp>
2020-04-04 19:41:24 +0900

    코드를 고쳤습니다.

--- Robot.java
+++ Robot.java
@@ -7,22 +7,27 @@ import java.util.Scanner;
  */
 public class Robot
 {
-    public static void main(String[] args){
-        double speed = 10;
-        String color;
-        Scanner scanner = new Scanner(System.in);
-        
-        //robot = RobotWalk();
-    }
-
     public static void RobotWalk(String color, double speed)
     {
-       if(color == "green")
-           System.out.println("동작을 멈춥니다.");
-       else if(color == "yellow")
-           System.out.println("보행속도를 " + speed*1.2 + "km/h로 걷습니다.");
-       else
-           System.out.println("동작을 멈춥니다.");
+        //System.out.print(color);
+        if(color.equals("green")){
+            System.out.println("속도를 유지하고 보행합니다.");
+        }
+        else if(color.equals("yellow")){
+            System.out.println("보행속도를 " + speed*1.2 + "km/h로 걷습니다.");
+        }
+        else{
+            System.out.println("동작을 멈춥니다.");
+        }
+    }
+    
+    public static void main(String[] args){
+        double speed = 10;
+        while(true){
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("교통신호는 :");
+            String color = scanner.nextLine();
+            RobotWalk(color,speed);
+        }
     }
 }
-
