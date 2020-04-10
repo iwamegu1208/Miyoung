ac1c131bd4f0c6842f82be8aafe016f10386b3e0
shka88 <lky1732@gmail.com>
2020-04-10 15:11:10 +0900

    변경2

--- RobotTest.java
+++ RobotTest.java
@@ -1,6 +1,6 @@
 //import java.util.Scanner;
 /**
- * 교통신호에 따라 RobotWalk 속도가 변경되는 프로그램
+ * 교통신호에 따라 RobotWalk 속도가 변경되는 프로그램입니다.
  * 
  * @author (2018315030 이가영,2018315059 이와사키 메구미) 
  * @version (2020.04.10)
